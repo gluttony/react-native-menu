e220c95af07e75c4a527b8c9ec72772dfa041164
gluttony <gluttony@users.noreply.github.com>
2024-09-16 16:13:13 +0800

    feat: build ts type

--- lib/typescript/module/src/UIMenuView.ios.d.ts
+++
@@ -1,5 +0,0 @@
-import { type HostComponent } from 'react-native';
-import type { NativeMenuComponentProps } from './types';
-declare const MenuComponent: HostComponent<NativeMenuComponentProps>;
-export default MenuComponent;
-//# sourceMappingURL=UIMenuView.ios.d.ts.map
